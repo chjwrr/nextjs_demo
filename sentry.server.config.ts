9043946861c468c5881d2ad4652a4d2fa8303e6d
尼古拉斯常 <1119577587@qq.com>
2023-08-23 11:54:41 +0800

    add ethers

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://671250a2068e92096df91747ca94c4e6@o4505752206049280.ingest.sentry.io/4505752209326080",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
